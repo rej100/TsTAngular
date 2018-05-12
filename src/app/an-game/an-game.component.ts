cd180a1e4f88c6f9e653d01269ab4846af5a5ee8
rej100 <srej100@gmail.com>
2018-05-12 21:44:08 +0200

    them changes

--- src/app/an-game/an-game.component.ts
+++ src/app/an-game/an-game.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Canon } from "./gClasses";
+import { Canon, CanonBall, Enemy } from "./gClasses";
+import { HostListener } from '@angular/core';
 
 
 
@@ -7,17 +8,26 @@ import { Canon } from "./gClasses";
 @Component({
   selector: 'app-an-game',
   templateUrl: './an-game.component.html',
-  styleUrls: ['./an-game.component.css']
+  styleUrls: ['./an-game.component.css'],
+  host: {
+    '(document:keypress)': 'handleKeyboardEvent($event)'
+  }
 })
 
-export class ANGameComponent implements OnInit {
-
+export class ANGameComponent implements OnInit
+{
   canvas: any;
   c: any;
   mousex: number;
   mousey: number;
+  key: any = "";
 
   mainCanon: Canon;
+  ballz: CanonBall[] = [];
+  enemies: Enemy[] = [];
+
+  canShoot: boolean = true;
+  score: number = 0;
 
   constructor()
   {
@@ -45,18 +55,96 @@ export class ANGameComponent implements OnInit {
     this.animate();
   }
 
+  getDist(ox: number, oy: number, tx: number, ty: number)
+  {
+    let distance: number = Math.sqrt( Math.pow((ox - tx), 2) + Math.pow((oy - ty), 2) );
+    return distance; 
+  }
+
   constructProps()
   {
-    this.mainCanon = new Canon(800, 700, 50, 80, this.c, 16, 5);
+    this.mainCanon = new Canon(800, 850, 50, 80, this.c, 16, 5, "white");
+    //console.log(this.getDist(3, 2, 2, 2));
+    //this.enemies.push( new Enemy( 500, 250, 16, this.c, 0, 0, "red", 6 ) );
   }
+
 
   animate()
   {
     requestAnimationFrame(() => {this.animate()});
+
     this.c.fillStyle = "rgba(0, 0, 0, 0.2)";
     this.c.fillRect(0, 0, innerWidth, innerHeight);
-    this.c.fillStyle = "white";
+    this.c.fillStyle = "rgba(255, 255, 255, 1)";
+    this.c.font = "30px Arial";
+
+    this.c.fillText("SCORE: " + this.score.toString(), 50, 160);
     this.mainCanon.update(this.mousex);
+
+    //there's a 3 in a 500 chance that an enemy will spawn; The x and the y cords are generated randomly for every enemy.
+    if(Math.floor(Math.random() * 500) <= 3)
+    {
+      this.enemies.push( new Enemy( (Math.floor( Math.random() * (innerWidth - 200) ) + 100), (Math.floor( Math.random() * (innerHeight - 700) ) + 200), 16, this.c, 0, 0, "red", 6 );   )
+    }
+
+    for(var i = 0; i < this.ballz.length; ++i)
+    {
+      if(this.enemies.length > 0)
+      {
+        let j: number = 0; 
+        while(j < this.enemies.length)
+        {
+          if(this.getDist(this.ballz[i].x, this.ballz[i].y, this.enemies[j].x, this.enemies[j].y) <= ( this.enemies[j].radius + this.ballz[i].radius ))
+          {
+            this.ballz[i].shouldDraw = false;
+            this.enemies[j].shouldDraw = false;
+          }
+          ++j;
+        }
+        
+      }
+      if(this.ballz[i].y <= 0)
+      {
+        this.ballz[i].shouldDraw = false;
+      }
+
+      this.ballz[i].update();
+    }
+    for(var i = 0; i < this.ballz.length; ++i)
+    {
+      if(!(this.ballz[i].shouldDraw))
+      {
+        this.ballz.splice(i, 1);
+      }
+    }
+    for(var i = 0; i < this.enemies.length; ++i)
+    {
+      this.enemies[i].update();
+    }
+    for(var i = 0; i < this.enemies.length; ++i)
+    {
+      if(!this.enemies[i].shouldDraw)
+      {
+        this.enemies.splice(i, 1);
+        ++this.score;
+      }
+    }
+  }
+
+  handleKeyboardEvent(event: KeyboardEvent)
+  { 
+    if(event.key === "h")
+    {
+      if(this.canShoot)
+      {
+        this.ballz.push( new CanonBall( (this.mainCanon.x + ( this.mainCanon.sizex / 2 ) ), this.mainCanon.y, 10, this.c, 0, -10, "lime") );
+        this.canShoot = false;
+        setTimeout(() =>
+        {
+          this.canShoot = true;
+        }, 250);
+      }
+    }
   }
 
 }
--- src/app/an-game/gClasses.ts
+++ src/app/an-game/gClasses.ts
@@ -8,8 +8,9 @@ export class Canon
   
   vx: number;
   vy: number
+  color: string;
   //cnx is the 2d context of a canvas element
-  constructor(x: number, y: number, sizex: number, sizey: number, cnx: any, vx: number, vy: number)
+  constructor(x: number, y: number, sizex: number, sizey: number, cnx: any, vx: number, vy: number, color: string)
   {
     this.x = x;
     this.y = y;
@@ -18,9 +19,11 @@ export class Canon
     this.cnx = cnx;
     this.vx = vx;
     this.vy = vy;
+    this.color = color;
   }
   draw()
   {
+    this.cnx.fillStyle = this.color;
     this.cnx.fillRect(this.x, this.y, this.sizex, this.sizey);
   }
   update(toFollowX: number)
@@ -75,17 +78,81 @@ export class CanonBall
   y: number;
   radius: number;
   cnx: any;
+  shouldDraw: boolean;
   
   vx: number;
   vy: number;
+  color: string;
   //cnx is the 2d context of a canvas element
-  constructor(x: number, y: number, radius: number, cnx: any, vx: number, vy: number)
+  constructor(x: number, y: number, radius: number, cnx: any, vx: number, vy: number, color: string)
   {
+    this.shouldDraw = true;
     this.x = x;
     this.y = y;
     this.radius = radius;
     this.cnx = cnx;
     this.vx = vx;
     this.vy = vy;
+    this.color = color;
+  }
+  draw()
+  {
+    if(this.shouldDraw)
+    {
+      this.cnx.fillStyle = this.color;
+      this.cnx.beginPath();
+      this.cnx.arc(this.x, this.y, this.radius, 0 , 2*Math.PI);
+      this.cnx.fill();
+    }
+  }
+  update()
+  {
+    this.x += this.vx;
+    this.y += this.vy;
+    this.draw();
+  }
+}
+
+export class Enemy
+{
+  x: number;
+  y: number;
+  radius: number;
+  cnx: any;
+  shouldDraw: boolean;
+  
+  vx: number;
+  vy: number;
+  color: string;
+  lineWidth: number;
+  //cnx is the 2d context of a canvas element
+  constructor(x: number, y: number, radius: number, cnx: any, vx: number, vy: number, color: string, lineWidth: number)
+  {
+    this.shouldDraw = true;
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.cnx = cnx;
+    this.vx = vx;
+    this.vy = vy;
+    this.color = color;
+    this.lineWidth = lineWidth;
+  }
+  draw()
+  {
+    if(this.shouldDraw)
+    {
+      this.cnx.lineWidth = this.lineWidth;
+      this.cnx.strokeStyle = this.color;
+      this.cnx.beginPath();
+      this.cnx.arc(this.x, this.y, this.radius, 0, 2*Math.PI);
+      this.cnx.stroke();
+    }
+  }
+  update()
+  {
+    this.x += this.vx;
+    this.y += this.vy;
+    this.draw();
   }
 }
