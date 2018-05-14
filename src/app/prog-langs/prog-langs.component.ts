4f93e388ce85ad14b52c40df27fb1206e4c84b85
rej100 <srej100@gmail.com>
2018-05-15 00:45:55 +0200

    a lot of images

--- src/app/prog-langs/prog-langs.component.ts
+++ src/app/prog-langs/prog-langs.component.ts
@@ -23,13 +23,13 @@ export class ProgLangsComponent implements OnInit {
   [
     {
       name: "C++",
-      imgPath: "https://vignette.wikia.nocookie.net/glest/images/4/47/Placeholder.png/revision/latest?cb=20101114155552",
+      imgPath: "https://png.kisspng.com/sh/e18690b1c2da6e2010bfdc8910de32dc/L4Dxd3E5UME5OGU1UJHyaIeye7r6kCBvb155gNc2Yz3zgrFukvFudZpzf591YX7qhbLuhb1kd551jeZucj3sc7F1k71kd551jeY2cILyd8Pojf1qdpgyTdNsYXTmQra4VvVnP2kzSqo5Nki8RoW4VcI0OmU5SKkDMEm3PsH1h5==/kisspng-the-c-programming-language-computer-icons-comput-programming-5acadc2e16ef78.280689641523244078094.png",
       pros: ["Fast", "Robust", "Efficient in making video game cheats"],
       cons: ["Hard", "Hard to implement in web development"]
     },
     {
       name: "PHP",
-      imgPath: "https://vignette.wikia.nocookie.net/glest/images/4/47/Placeholder.png/revision/latest?cb=20101114155552",
+      imgPath: "http://php.net/images/logos/new-php-logo.png",
       pros: ["......", "WTF?"],
       cons: ["Slow", "Deprecated", "Retarded", "Does not comply to any standards whatsoever", "Archaic",]
     }
