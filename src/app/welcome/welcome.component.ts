e0cf55613bc70a0554d42cf1fdc716a59abbbcd3
rej100 <srej100@gmail.com>
2018-05-21 23:09:41 +0200

    the slider's fucking done

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -6,11 +6,88 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.css']
 })
-export class WelcomeComponent implements OnInit {
 
+export class WelcomeComponent implements OnInit
+{
+
+  slideWidth: number = 640;
+  id: any;
+  pos: any;
+  oldPos: any;
+  canSlide: boolean = true;
+  slideSpeed: number = 25;
+  slideCounter: number = 0;
+
+  slides =
+  [
+    {url: "https://i.imgur.com/rOw0agT.jpg"},
+    {url: "https://i.imgur.com/xYnqbAL.jpg"},
+    {url: "https://i.imgur.com/0r7qN8U.jpg"},
+    {url: "https://i.imgur.com/uGfFixS.jpg"},
+    {url: "https://i.imgur.com/aBONniI.jpg"},
+    {url: "https://i.imgur.com/9CLFTW2.jpg"},
+  ]
+  
   constructor() { }
-
   ngOnInit() {
+  }
+
+  frame: any = (elem: any, direction: string) =>
+  {
+    if(direction === "left")
+    {
+      if(this.pos <= (this.oldPos - this.slideWidth))
+      {
+        clearInterval(this.id);
+        this.pos = this.oldPos - this.slideWidth;
+        this.canSlide = true;
+      }
+      else
+      {
+        this.pos -= this.slideSpeed;
+        elem.style.left = this.pos + "px";
+      }
+    }
+    if(direction === "right")
+    {
+      if(this.pos >= (this.oldPos + this.slideWidth))
+      {
+        clearInterval(this.id);
+        this.pos = this.oldPos + this.slideWidth;
+        this.canSlide = true;
+      }
+      else
+      {
+        this.pos += this.slideSpeed;
+        elem.style.left = this.pos + "px";
+      }
+    }
+    elem.style.left = this.pos + "px";
+    console.log(this.pos, this.oldPos);
+  }
+
+  slide(direction: string)
+  {
+    if( (this.canSlide && (((this.slideCounter - 1) >= 0) && (direction === "right"))) || (this.canSlide && ((this.slideCounter + 1) <= (this.slides.length - 1) && (direction === "left"))))
+    {
+      this.canSlide = false;
+      if(direction === "left")
+      {
+        ++this.slideCounter;
+      }
+      if(direction === "right")
+      {
+        --this.slideCounter;
+      }
+      let elem = document.getElementById("slider");
+
+      this.oldPos = elem.style.left;
+      this.oldPos = Number(this.oldPos.slice(0, -2));
+      this.pos = elem.style.left;
+      this.pos = Number(this.pos.slice(0, -2));
+
+      this.id = setInterval(this.frame, 1, elem, direction);
+    }
   }
 
 }
